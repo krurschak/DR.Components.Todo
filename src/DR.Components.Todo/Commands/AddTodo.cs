3b3269c06d44c98d3039c5473f01a48707afef1c
Krur Schak <krurschak@gmail.com>
2021-04-11 01:58:13 +0200

    Initial code commit

---
+++ src/DR.Components.Todo/Commands/AddTodo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DR.Components.Todo.Commands
+{
+    public interface AddTodo
+    {
+        Guid Id { get; }
+        Guid ProjectId { get; }
+        Guid CreatorId { get; }
+        string Name { get; }
+        string Description { get; }
+        Guid? AssignedUserId { get; }
+        DateTime? DueDateUtc { get; }
+        short Priority { get; }
+    }
+}
